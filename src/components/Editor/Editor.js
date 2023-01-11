4827e350388d6c84f95069daa2974447018f62a9
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-11 12:12:47 -0800

    blog title displaying in preview

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({ title, setTitle }) {
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" />
+        <input name="title" type="text" onChange={titleHandler} />
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -8,11 +9,12 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
 
+  const [title, setTitle] = useState('');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} setTitle={setTitle} />
+      <Editor title={title} setTitle={setTitle} />
     </main>
   );
 }
